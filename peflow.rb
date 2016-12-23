a068872166dab1d87c9cc3aa1f52c25b9d361263
foostan <ks@fstn.jp>
2016-12-24 02:16:53 +0900

    GET STDIN

--- peflow.rb
+++ peflow.rb
@@ -17,24 +17,32 @@ caps = Selenium::WebDriver::Remote::Capabilities.chrome({
 })
 
 driver = Selenium::WebDriver.for :chrome, desired_capabilities: caps
-driver.get 'https://www.google.com'
 
-puts '# show available types'
-puts driver.manage.logs.available_types
+while true
+  print '> '
+  url = STDIN.gets
+  break if url.chomp == "exit"
 
-puts '# show performance logs'
-pp driver.manage.logs.get(:performance)
-    .map{|log| JSON.parse(log.message)['message']}
-    .select{|log| log['method'] == 'Network.responseReceived'}
-    .map{|log| {
-      timestamp: log.dig('params', 'timestamp'),
-      url: log.dig('params', 'response', 'url'),
-      method: log.dig('params', 'response', 'requestHeaders', ':method'),
-      status: log.dig('params', 'response', 'status'),
-      protocol: log.dig('params', 'response', 'protocol'),
-      type: log.dig('params', 'type'),
-      connectionId: log.dig('params', 'response', 'connectionId'),
-      timing: log.dig('params', 'response', 'timing')}
-    }
+  puts "GET #{url}"
+  driver.get url
+
+  puts '# show available types'
+  puts driver.manage.logs.available_types
+
+  puts '# show performance logs'
+  pp driver.manage.logs.get(:performance)
+      .map{|log| JSON.parse(log.message)['message']}
+      .select{|log| log['method'] == 'Network.responseReceived'}
+      .map{|log| {
+        timestamp: log.dig('params', 'timestamp'),
+        url: log.dig('params', 'response', 'url'),
+        method: log.dig('params', 'response', 'requestHeaders', ':method'),
+        status: log.dig('params', 'response', 'status'),
+        protocol: log.dig('params', 'response', 'protocol'),
+        type: log.dig('params', 'type'),
+        connectionId: log.dig('params', 'response', 'connectionId'),
+        timing: log.dig('params', 'response', 'timing')}
+      }
+end
 
 driver.close
